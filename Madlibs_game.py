2c475a6366f29015f62cc1223dccc184e4899444
Mahek Dalal <mahekdalal14@gmail.com>
2026-01-17 19:00:43 +0530

    A game of words to make a story.

---
+++ Madlibs_game.py
@@ -0,0 +1,12 @@
+# it is a game where we make story out of random words
+adjective1=input("enter an adjective:")
+noun1=input("enter a noun:")
+adjective2=input("enter an adjective:")
+verb=input("enter a verb:")
+adjective3=input("enter an adjective:")
+
+
+print(f"\nToday i went to a {adjective1} zoo.")
+print(f"In an exibit I saw a {noun1}")
+print(f"{noun1} was {adjective2} and {verb}")
+print(f"I was {adjective3}")
